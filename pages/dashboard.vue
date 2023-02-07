fb40c1b6c613b462b6407f598d30efeffcfabf4d
Travis Reynertson <leicr@byu.edu>
2023-02-06 18:20:38 -0700

    working on stupid row col problem

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,9 +1,22 @@
 <template>
-    <v-col justify="center" align="center" class="dashboard_background">
-        <div class="bravo">Bravo!</div>
-        <div class="title">Title Board</div>
-        <post></post>
-    </v-col>
+    <section>
+        <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+        </head>
+        <body>
+
+            <v-col justify="center" align="center" class="dashboard_background">
+                <div class="bravo">Bravo!</div>
+                <div class="title-board">Title Board</div>
+                <div align="left">
+                    <post></post>
+                </div>
+            </v-col>
+
+        </body>
+    </section>
 </template>
 
 <script>
@@ -18,7 +31,28 @@ export default {
 
     data() {
         return {
-
+            post: [
+                {
+                    image: "No Image Available",
+                    message: "Piss off",
+                    author: "Billy B. Bob"
+                },
+                {
+                    image: "No Image Available",
+                    message: "Congrats",
+                    author: "Carl"
+                },
+                {
+                    image: "No Image Available",
+                    message: "Take a Hike",
+                    author: "Tom"
+                },
+                {
+                    image: "No Image Available",
+                    message: "You're worse than Weird Al",
+                    author: "Weird Al"
+                }
+            ]
         };
     },
 }
@@ -31,8 +65,13 @@ export default {
     .bravo {
         color: black;
         background-color: white;
+        padding: 10px;
+        border: 4px solid red;
     }
-    .title {
-        background-color: #585858;
+    .title-board {
+        /* background-color: #585858; */
+        background-color: pink;
+        padding: 20px;
+        border: 4px solid red;
     }
 </style>
