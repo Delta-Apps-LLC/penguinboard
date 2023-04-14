c6a40553657ae01533313f548c8b008a7702ccde
athibau2 <a.thibs98@gmail.com>
2023-04-14 13:16:13 -0600

    fixed file paths to privacy policy and terms

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,8 +4,8 @@
       app
     >
       <span>&copy; 2023 Delta Apps, LLC</span>
-      <a target="_blank" href="privacy_policy.pdf">Privacy Policy</a>
-      <a target="_blank" href="terms_conditions.pdf">Terms and Conditions</a>
+      <a target="_blank" href="/privacy_policy.pdf">Privacy Policy</a>
+      <a target="_blank" href="/terms_conditions.pdf">Terms and Conditions</a>
       <v-spacer />
       <a target="_blank" href="https://forms.gle/rWfHuxtMp37iDjiJA">Give Us Feedback</a>
     </v-footer>
