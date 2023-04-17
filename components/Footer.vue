b454d7f3e6c5d87984bb9868d3de95c1179b5d7a
athibau2 <a.thibs98@gmail.com>
2023-04-17 15:24:31 -0600

    added iframe for pdf in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,12 +4,44 @@
       app
     >
       <span>&copy; 2023 Delta Apps, LLC</span>
-      <a target="_blank" href="/privacy_policy.pdf">Privacy Policy</a>
-      <a target="_blank" href="/terms_conditions.pdf">Terms and Conditions</a>
+      <!-- <a target="_blank" href="/privacy_policy.pdf">Privacy Policy</a>
+      <a target="_blank" href="/terms_conditions.pdf">Terms and Conditions</a> -->
+      <v-btn @click="showPrivacy = !showPrivacy" text>Privacy Policy</v-btn>
+      <v-btn @click="showTerms = !showTerms" text>Terms and Conditions</v-btn>
+      <iframe v-if="showPrivacy" src="/privacy_policy.pdf" width="40%" height="30%"></iframe>
+      <iframe v-if="showTerms" src="/terms_conditions.pdf" width="40%" height="30%"></iframe>
       <v-spacer />
       <a target="_blank" href="https://forms.gle/rWfHuxtMp37iDjiJA">Give Us Feedback</a>
     </v-footer>
 </template>
+
+<script>
+export default {
+  name: 'Footer',
+
+  data () {
+    return {
+      showPrivacy: false,
+      showTerms: false,
+    }
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+    // pdfUrl () {
+    //   return require('~/static/privacy_policy.pdf')
+    // },
+
+    // termsUrl () {
+    //   return require('~/static/terms_conditions.pdf')
+    // }
+  }
+}
+</script>
+
 
 <style scoped>
 a {
