30e710fa2bf10010fe1542fbf409e26119cf7706
athibau2 <a.thibs98@gmail.com>
2023-04-17 16:36:23 -0600

    added html page for pdfs

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,12 +4,12 @@
       app
     >
       <span>&copy; 2023 Delta Apps, LLC</span>
-      <a target="_blank" :href="require(`~/static/privacy_policy.pdf`)">Privacy Policy</a>
-      <a target="_blank" :href="require(`~/static/terms_conditions.pdf`)">Terms and Conditions</a>
-      <v-btn @click="showPrivacy = !showPrivacy" text>Privacy Policy</v-btn>
+      <a target="_blank" href="privacy_policy.html">Privacy Policy</a>
+      <a target="_blank" href="terms_conditions.pdf">Terms and Conditions</a>
+      <!-- <v-btn @click="showPrivacy = !showPrivacy" text>Privacy Policy</v-btn>
       <v-btn @click="showTerms = !showTerms" text>Terms and Conditions</v-btn>
       <iframe v-if="showPrivacy" :src="require(`~/static/privacy_policy.pdf`)" width="40%" height="30%"></iframe>
-      <iframe v-if="showTerms" :src="require(`~/static/terms_conditions.pdf`)" width="40%" height="30%"></iframe>
+      <iframe v-if="showTerms" :src="require(`~/static/privacy_policy.pdf`)" width="40%" height="30%"></iframe> -->
       <v-spacer />
       <a target="_blank" href="https://forms.gle/rWfHuxtMp37iDjiJA">Give Us Feedback</a>
     </v-footer>
